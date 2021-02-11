3b91c907c10ebbdcaddda03229dd892c2d32b87d
johonescal <74693813+johonescal@users.noreply.github.com>
2021-02-11 13:45:14 +0900

    learn dir upload

---
+++ Makepass.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.math.BigInteger;
+import java.security.MessageDigest;	
+
+public class Makepass
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		//定数を宣言（Asciiコード）
+		final int Asc_exc = 33;
+		final int Asc_0 = 48;
+		final int Asc_9 = 57;
+		final int Asc_A = 65;
+		final int Asc_Z = 90;
+		final int Asc_a = 97;
+		final int Asc_z = 122;
+		final int Asc_til = 126;
+
+		//入力を受ける
+		int len = 0;
+		String mark = "";
+		while(true)
+		{
+			try {
+				System.out.println("パスワードの桁数を入力");
+				len = Integer.parseInt(scan.next());
+				break;
+			}catch(NumberFormatException e) {
+				System.out.println("数字で入力してください");
+				continue;
+			}
+		}
+		
+		System.out.println("記号：有_y   無_n");
+		mark = scan.next();
+		scan.close();
+
+		int i = 0;
+		char[] pass = new char[len];
+		
+		Random random = new Random();
+		if(mark.equals("y") || mark.equals("Y")){
+			while(i < len){
+				int rand = random.nextInt(Asc_til) + Asc_exc;
+					pass[i] = (char)rand;
+					i++;
+				}
+		}else if(mark.equals("n") || mark.equals("N")){
+			while(i < len) {
+				int rand = random.nextInt(Asc_z) + Asc_0;
+				if(rand >= Asc_0 && rand <= Asc_9 || rand >= Asc_A && rand <= Asc_Z || rand >= Asc_a && rand <= Asc_z) {
+					pass[i] = (char)rand;
+
+					i++;
+				}
+			}
+		}
+		
+		System.out.println(pass);
+		
+		String pastr = String.valueOf(pass);
+        	String pasha = "";
+		
+		try {
+            	MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            	byte[] result = digest.digest(pastr.getBytes());
+            	pasha = String.format("%040x", new BigInteger(1, result));
+        	} catch (Exception e){
+            		e.printStackTrace();
+        	}
+		
+		System.out.println(pasha);
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,57 @@
+# ユーザエラー自己解決プログラム（Errsettle.java)
+　Linuxコマンドの実行時にエラーが発生した場合、エラーの原因を解析し、エラーを解決するための処理を実行します。
+ 
+ このプログラムは、`mkdir`　と　`rmdir`　の実行時に発生したエラーだけを解決することができます。
+ 
+ → https://github.com/yowayowahacker/learn/blob/master/Errsettle.java
+
+
+# パスワード自動生成（autopass.py)
+　乱数を発生させ、Asciiコードをから文字に変換、8回繰り返して8文字のパスワードを作成する。
+ 作成したパスワードに用途とアカウント名を与えて、ファイルに保存する。
+
+欠けている機能
+- パスワードを保存するときに暗号化をせずに保存している。
+- 保存しているパスワードと被る可能性がある。
+
+ → https://github.com/yowayowahacker/learn/blob/master/autopass.py
+ ## (Makepass.java)
+ Java版です
+ → https://github.com/yowayowahacker/learn/blob/master/Makepass.java
+
+# Rot13（Rot13.py）
+　Rot13をPythonで書いたものです。
+ 入力された文字列を文字ごとにAsciiコードに直して、コード + 13　をして、文字に戻す処理をしています。
+ +13をしたときにA~Zに収まらないものは-26をしてうまく回転するようにしています。
+
+→ https://github.com/yowayowahacker/learn/blob/master/rot13.py
+
+# Javaのデバックツールを作る
+　ものすごく途中で未完成品ですが、これは、難しいデバックツールが使えないため　⇒　System.out.println("A");を入れる
+ 
+ これを手作業でやるととても面倒なので自動化しようというものです。
+ 
+ 進捗としては、編集するファイルを読み込んだところです。
+ 
+ このあとやらないといけないのは、
+ readlinkを使ったPATH取得の自動化,
+ 文字列の置き換え処理,
+ （例外処理を書くこと）
+
+勉強会で、linuxコマンドのmakeとsedを駆使すればなんとかなるのでは、、、という話
+
+---作成中---
+
+Linuxコマンドを利用して何とかする方法を実装してみました。
+
+→ https://github.com/yowayowahacker/learn/blob/master/Linux_Debug.java
+
+# 計算問題出題
+　基本的な例外処理を勉強しました。
+
+　割り算の解答入力の形式をどうしようか迷っています。
+ 
+　正答率を出す計算がうまく行かず困っています。
+
+　→ https://github.com/yowayowahacker/learn/blob/master/Cal.java
+
